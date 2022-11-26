267916354c42b166f218e72ab9de71170e78b33b
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-11-26 13:08:35 +0100

    test csv parse

--- routes/csv.js
+++ routes/csv.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
+const path = require('path');
+const {parse} = require('csv-parse');
+const fs = require('fs');
 
 router.get('/tp1', (req, res) => {
+
+const csvData = [];
+
+fs.createReadStream(path.join(__dirname,'../','/test.csv'))
+  .pipe(
+    parse({
+      delimiter: ','
+    })
+  )
+  .on('data', function (dataRow) {
+    csvData.push(dataRow);
+  })
+  .on('end', function () {
+    console.log(csvData);
+  })
   res.send('tp1');
 });
 
