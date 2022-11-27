9ade1ee84903fa5972ecfca1c39fe8a6114cf6d3
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-11-27 19:24:44 +0100

    tp done without unzip

--- routes/csv.js
+++ routes/csv.js
@@ -3,23 +3,57 @@ const path = require('path');
 const {parse} = require('csv-parse');
 const fs = require('fs');
 
-router.get('/tp1', (req, res) => {
+router.get('/tp1', async (req, res) => {
 
-const csvData = [];
+  const datas = [];
 
-fs.createReadStream(path.join(__dirname,'../','/test.csv'))
-  .pipe(
-    parse({
-      delimiter: ','
+  fs.createReadStream(path.join(__dirname,'../','/StockEtablissementLiensSuccession_utf8.csv'))
+    .pipe(
+
+      parse({
+
+        delimiter: ',',
+        from: 1,
+        to: 100000,
+        columns: true
+
+      })
+    )
+
+    .on('data', function (data) {
+
+      const transfert_sieges = {
+        value: data.transfertSiege,
+        date: data.dateDernierTraitementLienSuccession
+      }
+      datas.push(transfert_sieges);
+
     })
-  )
-  .on('data', function (dataRow) {
-    csvData.push(dataRow);
-  })
-  .on('end', function () {
-    console.log(csvData);
-  })
-  res.send('tp1');
-});
+
+    .on('end', function () {
+
+      // Setup variables for percentage calc
+      var count_rows = 0;
+      var count_transfert_sieges = 0;
+
+      // Loop on datas for counting both variables
+      datas.forEach((data) => {
+
+        console.log(data.date);
+        if (data.date < Date('2022-11-01')) {
+            count_rows += 1;
+
+          if (data.value == "true") {
+            count_transfert_sieges += 1;
+          }
+        }
+      })
+
+      // Return the percentage
+      res.send(`${count_transfert_sieges * 100 / count_rows}`);
+
+    })
+  }
+);
 
 module.exports = router;
